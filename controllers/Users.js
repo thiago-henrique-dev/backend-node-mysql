95d0ed1b166c47e0dbb8cbac795b32a7e1b49e7b
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-09-29 11:06:24 -0300

    Usuario cadastrado no banco de dados

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,4 +1,6 @@
 const mysql = require('../config/db');
+const validator = require('email-validator')
+
 
 exports.getUsers = async (req, res, next) => {
     mysql.query("SELECT * FROM usuarios", (err, rows) => {
@@ -13,3 +15,37 @@ exports.getUsers = async (req, res, next) => {
       }
     });
   };
+
+  exports.save = async (req, res) => {
+    const { id_usuario, nome, sobrenome, email, telefone, cpf } = req.body;
+    const emailValid = validator.validate(email);
+  
+    if(nome === '' || nome === 0){
+      return res.status(400).send({
+        err: "This name cannot be entered"
+    })
+    } 
+    else if(!emailValid){
+      return res.status(400).send({
+        err: "Email already in use"
+    })
+  
+    }
+    mysql.query(
+      "INSERT INTO usuarios (id_usuario, nome, sobrenome, email, telefone, cpf) VALUES (default,?,?,?,?,?)",
+      [id_usuario, nome, sobrenome, email, telefone, cpf,],
+      (err, rows) => {
+        
+        if (err) {
+          return res.status(400).send({
+            err,
+          });
+        } else {
+          return res.status(200).send({
+            message: "User successfully created",
+          });
+        }
+      }
+    );
+  };
+  
--- routes.js
+++ routes.js
@@ -6,3 +6,4 @@ module.exports = router;
 const users = require('./controllers/Users')
 
 router.get('/users', users.getUsers)
+router.post('/register', users.save)
