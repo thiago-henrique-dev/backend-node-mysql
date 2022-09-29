daeddeff229ddb2dd8fa6d3c978261feef67e06c
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-09-29 10:42:06 -0300

    conectando ao banco de dados, criando os controllers getUsers e routas getUsers

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,2 +1,15 @@
 const mysql = require('../config/db');
 
+exports.getUsers = async (req, res, next) => {
+    mysql.query("SELECT * FROM usuarios", (err, rows) => {
+      if (err) {
+        return res.status(400).send({
+          err,
+        });
+      } else {
+        return res.status(200).send({
+          data: rows,
+        });
+      }
+    });
+  };
--- routes.js
+++ routes.js
@@ -3,4 +3,6 @@ const router = express.Router();
 
 module.exports = router;
 
+const users = require('./controllers/Users')
 
+router.get('/users', users.getUsers)
